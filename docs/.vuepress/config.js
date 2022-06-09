9b5619e091c4a58dbc05b48cca998344c9837aa9
archaemedes <wadecroft@gmail.com>
2022-06-08 20:47:55 -0700

    configure site

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,14 @@
+const { defaultTheme } = require('@vuepress/theme-default')
+
 module.exports = {
-  lang: 'en-US',
-  title: 'Stikky Wiki',
-  description: 'This is my first VuePress site',
+  head: [['link', { rel: 'icon', href: 'https://www.stikkymedia.com/wp-content/uploads/2020/02/stikkyicon-orange.png' }]],
+  theme: defaultTheme({
+    logo: "https://www.stikkymedia.com/wp-content/uploads/2019/11/stikky-horzontal-logo.png",
+    lang: 'en-US',
+    title: 'Stikky Wiki',
+    description: 'This is my first VuePress site',
+    contributors: false,
+    darkMode: true,
+    logoDark: "https://www.stikkymedia.com/wp-content/uploads/2021/01/stikkylogo-sideways-white-1.png"
+  })
 }
